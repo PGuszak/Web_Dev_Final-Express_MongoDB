14b150bbfe37bca6aa9465051da0dc18c000f458
pguszak <pguszak@yahoo.com>
2021-04-13 20:51:33 -0600

    small changes

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 //"C:\Users\gusza\OneDrive\Desktop\College\Senoir Year\Spring 2021\Special Topics\Web_Dev_Final_Project+Exp_Node_MDB"
-
-const layouts = require("express-ejs-layouts");
-
 //use CTRL+SHIFT+M for seeing readme preview
 const express = require("express"), 
 app = express(),
@@ -9,7 +6,6 @@ router = express.Router(),
 homeController = require("./controllers/homeControllers"),
 errorController = require("./controllers/errorController"),
 userController = require("./controllers/usersControllers"),
-methodOverride = require("method-override"),
 layouts = require("express-ejs-layouts"),
 methodOverride = require("method-override"),
 passport = require("passport"),
@@ -23,12 +19,13 @@ mongoose = require("mongoose");
 
 //mongoose connection
 mongoose.connect("mongodb://localhost:27017/Orbit_Users", { useNewUrlParser: true });
-mongoose.set("userCreateIndex", true);
+mongoose.set("useCreateIndex", true);
 const db = mongoose.connection;
 db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 });
-//*********************
+
+
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -36,21 +33,26 @@ router.use(
     express.urlencoded({ 
         extended: false,
     })
+);
 
-    );
+router.use(cookieParser("mypasscode"));
+
+
 router.use(express.json());
 app.use(express.static(__dirname + '/public'));  //so we can access the public folder
+
+
 
 //preprosessing 
 router.use(express.static("public"));  //can serve static content to users
 router.use(layouts);
 app.use(methodOverride("_method", {methods: ["POST", "GET", "PUT"]}));
 app.use("/", router);
-//*******************
 
 
+
+//normal routes
 router.get("/", homeController.showSignIn); //this is what renders first in the layout.ejs file
-
 
 router.get("/signup", homeController.showSignUp);
 router.post("/signup", userController.saveUser);
